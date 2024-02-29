eb31bc91485b7790f374c54d422e9cd2ee8b965f
Mert Durkaya <mertdurkaya@gmail.com>
2024-02-29 17:12:30 +0300

    Initial Commit

---
+++ Mapy/MapyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapyApp.swift
+//  Mapy
+//
+//  Created by Mert Durkaya on 29/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MapyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
